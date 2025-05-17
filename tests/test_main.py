0bea888852602a384284360e6b2b1a93e8ab200e
Supawin Pitichotenant <supawin.pit@gmail.com>
2025-05-17 15:20:49 +0700

    update

--- tests/test_main.py
+++ tests/test_main.py
@@ -6,7 +6,7 @@ import sys
 # Add the parent directory to the path so we can import from the main app
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
-from main123 import app
+from main import app
 
 # Create a test client
 client = TestClient(app)
